4221e0b508dae3b4c4df7cfa510c06d8cd297c66
ninaZh-dev <nina.l.l.zhang2@gmail.com>
2026-01-10 12:30:20 -0500

    front page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import drawing from './assets/drawing2.png'
 import './App.css'
 
 function App() {
@@ -9,24 +8,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="drawing2.png" target="_blank">
+          <img src={drawing} className="logo" alt="logo" />
         </a>
       </div>
-      <h1>testing react right now</h1>
+      <h1>HealthPal</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick="this.loginTrack=true">
+          Log-in Streak: {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Your friendly health tracker :)
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        We provide the most up to date information, to help YOU stay happy and health!
       </p>
     </>
   )
