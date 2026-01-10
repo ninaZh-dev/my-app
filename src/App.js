f05e7a9af56788ec318fbdd2e6722e7b9ea46c9c
Jayna <ongjayna@gmail.com>
2026-01-10 12:31:42 -0500

    tracker blueprint

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function logInput() {
+    let input = document.getElementById("input-value")
+    if(input.value ==! ""){
+        adjustList();
+    }
+}
+
+function adjustList(){
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './App.js'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <h3>Tracker</h3>
+        <input id="input-value" type="text" placeholder="Input your values here"></input>
+        <button id="enter-action" onclick="logInput()">Enter</button>
+      </div>
+      <div id="tracker">
+        <h3>Logs</h3>
+        <ul id="tracker-list">
+
+        </ul>
+      </div>
     </>
   )
 }
