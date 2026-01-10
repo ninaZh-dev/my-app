56fd73604004a365d48680621b74be6a36aebfdb
ninaZh-dev <nina.l.l.zhang2@gmail.com>
2026-01-10 14:17:53 -0500

    change2

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import drawing from './assets/drawing2.png'
 import './App.css'
-import './App.js'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    if (!clicked) {
+      setCount(count + 1)
+      setClicked(true)
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={drawing} className="logo" alt="logo" />
       </div>
-      <h1>testing react right now</h1>
+      <h1>HealthPal</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleClick} disabled={clicked}>
+          Log-in Streak: {count}
         </button>
+        {clicked && <p>You've logged in! Click the heart to go to your tracker.</p>}
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Your friendly health tracker :)
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        We provide the most up to date information, to help YOU stay happy and health!
       </p>
-      <div>
-        <h3>Tracker</h3>
-        <input id="input-value" type="text" placeholder="Input your values here"></input>
-        <button id="enter-action" onclick="logInput()">Enter</button>
-      </div>
-      <div id="tracker">
-        <h3>Logs</h3>
-        <ul id="tracker-list">
-
-        </ul>
-      </div>
     </>
   )
 }
